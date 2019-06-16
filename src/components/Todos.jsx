2268c4babf634c57568ba43b82a22cb6febf9e42
ezio <fahdmajzoub13@gmail.com>
2019-06-16 18:33:41 +0200

    app has been created

---
+++ src/components/Todos.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+class Todos extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+         
+          <h2>To Do List component</h2>
+        </div>
+        
+      </div>
+    );
+  }
+}
+
+export default Todos;
